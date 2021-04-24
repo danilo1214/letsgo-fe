0a410f34c4ecbbbfc656453b1cf98c6979111c29
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2021-04-24 12:36:39 +0200

    Navbar design finished

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,87 @@
 <template>
-  <v-app-bar app color="blue" dark>
-    <v-toolbar-title>Letsgo</v-toolbar-title>
+  <v-app-bar app height="140">
 
-    <v-spacer></v-spacer>
-    <v-text-field v-model="search" placeholder="Search Plans..."></v-text-field>
+    <v-text-field rounded filled dense clearable class="mr-5 mt-5" prepend-icon="mdi-magnify" v-model="search" placeholder="Search Plans..."></v-text-field>
 
-    <v-menu         ref="menu"
+
+    <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        left
+        bottom
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-card class="pa-5">
+        <v-list>
+          <v-list-item-title>
+            Filters
+          </v-list-item-title>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-range-slider
+                v-model="priceRange"
+                label="Price($)"
+                thumb-label="always"
+                class="mr-5"
+                max="100"
+                min="0"
+            >
+              <template v-slot:thumb-label="{value}">
+                {{ formatPrice(value) }}
+              </template>
+            </v-range-slider>
+
+          </v-list-item>
+
+          <v-list-item>
+            <v-menu ref="menu"
                     :close-on-content-click="false"
                     transition="scale-transition"
                     offset-y
                     min-width="auto"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-text-field
-            v-model="formatDates"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-          ref="picker"
-          v-model="dates"
-          range
-          :max="new Date().toISOString().substr(0, 10)"
-          min="1950-01-01"
-          @change="setDate"
-      ></v-date-picker>
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                    class="mr-5"
+                    v-model="formatDates"
+                    prepend-icon="mdi-calendar"
+                    placeholder="Select Dates..."
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                  ref="picker"
+                  v-model="dates"
+                  range
+                  :max="new Date().toISOString().substr(0, 10)"
+                  min="1950-01-01"
+                  @change="setDate"
+              ></v-date-picker>
+            </v-menu>
+
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-menu>
 
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+    <v-btn color="primary">
+      Search
     </v-btn>
   </v-app-bar>
 
@@ -41,20 +90,25 @@
 <script>
 export default {
   name: "NavBar",
-  data(){
+  data() {
     return {
+      menu: false,
       search: "",
-      dates: []
+      dates: [],
+      priceRange: [0,0]
     }
   },
   computed: {
-    formatDates(){
+    formatDates() {
       return this.dates.join(" - ");
     }
   },
   methods: {
-    setDate(dates){
+    setDate(dates) {
       this.$refs.menu.save(dates)
+    },
+    formatPrice(value) {
+      return `$${value}`;
     }
   }
 }
