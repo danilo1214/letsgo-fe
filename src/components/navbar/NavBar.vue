283338909252d03c1982c34d9cf10e66b95afa89
Danilo Dimitrievski <danilo@Danilo-Dimitrievskis-MacBook-Pro.local>
2022-05-24 14:14:16 +0200

    update

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -7,7 +7,7 @@
       label="Search by keywords..."
       :items="keywords"
       v-model="form.search"
-      @keyup.enter='onSearch'
+      @keyup.enter.native='onSearch'
     />
 
     <v-menu v-model="menu" :close-on-content-click="false" left bottom>
