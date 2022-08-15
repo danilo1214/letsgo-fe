13ee84736e316316cb2d302919949c8fa59a1b0c
Danilo Dimitrievski <danilo.dimitrievski@3fs.si>
2022-08-15 18:55:16 +0200

    sidebar

--- src/components/navbar/SideBar.vue
+++ src/components/navbar/SideBar.vue
@@ -5,38 +5,36 @@
     @input="(v) => $emit('input', v)"
     mobile-breakpoint="xs"
   >
-    <template v-if="signedIn">
-      <v-list-item class="px-2">
-        <Avatar :user="user" />
-        <v-list-item-title>{{ name }}</v-list-item-title>
-      </v-list-item>
+    <v-list-item class="px-2" v-if='signedIn'>
+      <Avatar :user="user" />
+      <v-list-item-title>{{ name }}</v-list-item-title>
+    </v-list-item>
 
-      <v-divider></v-divider>
+    <v-divider></v-divider>
 
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.link"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon v-if="item.title !== 'Friends' || !friendRequests.length">{{
+    <v-list dense>
+      <v-list-item
+        v-for="item in availableRoutes"
+        :key="item.title"
+        :to="item.link"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon v-if="item.title !== 'Friends' || !friendRequests.length">{{
               item.icon
             }}</v-icon>
-            <v-badge v-else :content="friendRequests.length">
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-            </v-badge>
-          </v-list-item-icon>
+          <v-badge v-else :content="friendRequests.length">
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-badge>
+        </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </template>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
 
     <div class="mt-5">
       <Button
@@ -74,20 +72,25 @@ export default {
   data() {
     return {
       items: [
-        { title: 'Home', icon: 'mdi-home-city', link: '/' },
-        { title: 'My Account', icon: 'mdi-account', link: '/account' },
-        { title: 'My Plans', icon: 'mdi-calendar', link: '/my-plans' },
-        { title: 'Friends', icon: 'mdi-account-group', link: '/friends' },
-        { title: 'Sign Out', icon: 'mdi-logout', link: '/sign-out' },
+        { title: 'Home', icon: 'mdi-home-city', link: '/', isPublic: true},
+        { title: 'My Account', icon: 'mdi-account', link: '/account', isPublic: false},
+        { title: 'My Plans', icon: 'mdi-calendar', link: '/my-plans', isPublic: false },
+        { title: 'Friends', icon: 'mdi-account-group', link: '/friends', isPublic: false },
+        { title: 'Sign Out', icon: 'mdi-logout', link: '/sign-out', isPublic: false },
       ],
     };
   },
   computed: {
     ...mapState({ user: (state) => state.auth.user }),
     ...mapGetters(['signedIn', 'friendRequests']),
+    availableRoutes() {
+      if(this.signedIn) {
+        return this.items;
+      }
+      return this.items.filter(item => item.isPublic);
+    },
     name() {
-      const { user } = this;
-      return `${user.first_name} ${user.last_name}`;
+      return `${this.user.first_name} ${this.user.last_name}`;
     },
   },
 };
